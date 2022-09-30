7adc1efdf07d2125490a4a23612ad60ce9783de2
Ahnnet <drj9802@gmail.com>
2022-09-30 21:19:43 +0900

    free2_cloud

--- 98_free2/Graphics02-WebGL/free2.js
+++ 98_free2/Graphics02-WebGL/free2.js
@@ -27,10 +27,30 @@ window.onload = function init()
         // m3
         vec2(0.5, -1.0),
         vec2(1.0, 0.3),
-        vec2(1.0, -1.0)
-        
+        vec2(1.0, -1.0),
 
-    ]
+        // cloud 1
+        vec2(0.0,0.0),
+        vec2(-0.2,-0.2),
+        vec2(0.2,-0.2),
+        vec2(0.3,0),
+        vec2(0.2,0.2),
+        vec2(-0.2,0.2),
+        vec2(-0.3,0),
+        vec2(-0.2,-0.2),
+
+        // cloud 2
+        //cloud 1
+        vec2(-0.7,0.75),
+        vec2(-0.8,0.6),
+        vec2(-0.6,0.6),
+        vec2(-0.45,0.75),
+        vec2(-0.6,0.9),
+        vec2(-0.8,0.9),
+        vec2(-0.95,0.75),
+        vec2(-0.8,0.6)
+
+    ];
 
     gl.viewport( 0, 0, canvas.width, canvas.height );   //canvas 넓이 재정의
     gl.clearColor( 0.0, 0.2, 0.5, 1.0 );    //배경색 지정
@@ -55,8 +75,8 @@ window.onload = function init()
     gl.uniform4fv( colors, [0.0, 1.0, 0.0, 1.0]);
     mountains();
 
-    // gl.uniform4fv( colors, [0.0, 1.0, 0.0]);
-    // clouds();
+    gl.uniform4fv( colors, [1.0, 1.0, 1.0, 1.0]);
+    clouds();
 
     // gl.uniform4fv( colors, [0.0, 1.0, 0.0]);
     // stars();
@@ -68,11 +88,13 @@ function mountains() {
     gl.drawArrays( gl.TRIANGLES, 0, 3 );
     gl.drawArrays( gl.TRIANGLES, 3, 3 );
     gl.drawArrays( gl.TRIANGLES, 6, 3 );
+
 }
 
-// function clouds() {
-
-// }
+function clouds() {
+    gl.drawArrays( gl.TRIANGLE_FAN, 9, 8);
+    gl.drawArrays( gl.TRIANGLE_FAN, 17, 8);
+}
 
 // function stars() {
 
--- test/Graphics02-WebGL/test.js
+++ test/Graphics02-WebGL/test.js
@@ -9,15 +9,25 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );   //canvas 넓이 재정의
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );    //배경색 지정
 
+    //var vertices = new Float32Array([vec2(-1, -1), vec2(0, 1), vec2(1, -1)]);
+	  // var vertices = [ vec2(-1,-1), vec2(0,1), vec2(1,-1)];
     //  Configure WebGL
+    
+    // var vertices = new Float32Array([-1, -1, -0.5, 1, 0, -1, 0, -1, 0.5, 1, 1, -1 ]);
     var vertices = [
-        vec2(-1.0, -1.0),
-        vec2(-0.5, 0.0),
-        vec2(0.0, -1.0)
+        vec2(0.0,0.0),
+        vec2(-0.2,-0.2),
+        vec2(0.2,-0.2),
+        vec2(0.3,0),
+        vec2(0.2,0.2),
+        vec2(-0.2,0.2),
+        vec2(-0.3,0),
+        vec2(-0.2,-0.2)
     ];
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );   //canvas 넓이 재정의
+    gl.clearColor( 0.0, 0.2, 0.5, 1.0 );    //배경색 지정
 
     //  Load shaders and initialize attribute buffers
 
@@ -26,19 +36,15 @@ window.onload = function init()
 
     // Load the data into the GPU
 
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.bufferData( gl.ARRAY_BUFFER,flatten(vertices), gl.STATIC_DRAW );
+
     // Associate vertex data buffer with shader variables
+
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
-
-
-    
-    var vColors = gl.getUniformLocation(program, "vColors");    
-    gl.uniform4fv( vColors, [1.0, 1.0, 0.0]);
-
 
     render();
 };
@@ -46,5 +52,5 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 8 );
 }
