48d51056c21b0825d40d0c079327d32b21871834
Ahnnet <drj9802@gmail.com>
2022-10-04 23:01:54 +0900

    assignment_result_screen

---
+++ 10_cube_rotate_fixed_point/Graphics02-WebGL/Graphics02-WebGL/cube_ctm.js
@@ -0,0 +1,143 @@
+
+var canvas;
+var gl;
+
+var NumVertices  = 36;
+
+var points = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [ -10.0, 10.0, 0.0-rot ];
+
+var ctm;
+var modelViewMatrixLoc;
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    colorCube();
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+   modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    
+    //event listeners for buttons
+    
+    document.getElementById( "Execute" ).onclick = function () {
+      render();
+    };
+        
+    render();
+}
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 ); // blue
+    quad( 2, 3, 7, 6 ); // yellow
+    quad( 3, 0, 4, 7 ); // green
+    quad( 6, 5, 1, 2 ); // cyan
+    quad( 4, 5, 6, 7 ); // red       
+    quad( 5, 4, 0, 1 ); // magenta
+}
+
+function quad(a, b, c, d) 
+{
+    var vertices = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ), // 0
+        vec4( -0.5,  0.5,  0.5, 1.0 ), // 1
+        vec4(  0.5,  0.5,  0.5, 1.0 ), // 2
+        vec4(  0.5, -0.5,  0.5, 1.0 ), // 3
+        vec4( -0.5, -0.5, -0.5, 1.0 ), // 4     // point P 
+        vec4( -0.5,  0.5, -0.5, 1.0 ), // 5
+        vec4(  0.5,  0.5, -0.5, 1.0 ), // 6
+        vec4(  0.5, -0.5, -0.5, 1.0 )  // 7
+    ];
+
+    var vertexColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+    ];
+
+    // We need to parition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+    
+    //vertex color assigned by the index of the vertex
+    
+    var indices = [ a, b, c, a, c, d ]; // 1 0 3, 1 3 2 // 4 5 6, 4 6 7
+
+   console.log(indices)
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        //colors.push( vertexColors[indices[i]] );
+    
+        // for solid colored faces use 
+        colors.push(vertexColors[a]);
+        
+    }
+}
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    theta[zAxis] += 2;    //rotate about z-axis
+    
+   ctm = mat4();
+
+    ctm = mult(ctm, translate(-0.5, -0.5, -0.5, 1.0))  // translation to initial location (verticies[4])
+
+   ctm = mult(ctm, rotate(theta[zAxis], 0, 0, 1)); // rotateZ
+    ctm = mult(ctm, rotate(theta[yAxis], 0, 1, 0)); // rotateY
+   ctm = mult(ctm, rotate(theta[xAxis], 1, 0, 0)); // rotateX
+
+    ctm = mult(ctm, translate(0.5, 0.5, 0.5, 1.0))  // translation to (0,0,0)
+
+   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm));
+
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+
+    requestAnimFrame( render );
+}
