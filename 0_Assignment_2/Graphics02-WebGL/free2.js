808d648b31ebeae108724b520efecc57f03ce89c
Ahnnet <drj9802@gmail.com>
2022-10-17 23:48:51 +0900

    Assignment2_finish translation (left/right) canvas click event (draw diamonds)

--- 0_Assignment_2/Graphics02-WebGL/free2.js
+++ 0_Assignment_2/Graphics02-WebGL/free2.js
@@ -1,14 +1,18 @@
+
 var gl;
 var points;
 var colors;
-
 var modelViewMatrix;
+
 var flag=0;
 var cloud_trans=0;
 var star_trans=0;
+
 var ctm;
 var ctm2;
 var ctm3;
+
+var index=0;
 
 window.onload = function init()
 {
@@ -145,7 +149,7 @@ window.onload = function init()
 
         vec2(0.48,-0.2),
         vec2(0.6,-0.35),
-        vec2(0.72,-0.2)
+        vec2(0.72,-0.2),
         
     ];
 
@@ -156,6 +160,7 @@ window.onload = function init()
 
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
+
 
     // create buffer and Load the data into the GPU
     var bufferId = gl.createBuffer();
@@ -170,24 +175,53 @@ window.onload = function init()
 
     modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
 
+    // right button
     document.getElementById("btnRight").onclick=function(){
         flag = 1;
         render();
     }
+    
+    //left button
     document.getElementById("btnLeft").onclick=function(){
         flag = -1;
         render();
     }
+
     render();
+
+    // click event listener
+    canvas.addEventListener("mousedown", function(event){
+        var x = 2*event.clientX/canvas.width-1;
+        var y = 2*(canvas.height-event.clientY)/canvas.height-1;
+        index = index+1;
+
+        var t = [
+            vec2(x,y),
+            vec2(x,y-0.025),
+            vec2(x+0.025,y),
+            vec2(x,y+0.025),
+            vec2(x-0.025,y),
+            vec2(x,y-0.025)
+        ]
+
+        vertices = vertices.concat(t);
+        gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+        render();
+
+        console.log(vertices);
+    });
 
 };
 
 function render(){
     gl.clear( gl.COLOR_BUFFER_BIT );
 
+    // translation amount
     cloud_trans+=0.002*flag;
     star_trans+=0.0005*flag;
 
+    // matrix for translateion
     ctm = mat4();
     ctm = mult(ctm, translate(cloud_trans, 0.0, 0.0, 0.0));
     ctm2 = mat4();
@@ -220,7 +254,7 @@ function render(){
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm));
     clouds2();
 
-    //draw stars with orange color
+    //draw giant stars with orange color
     gl.uniform3fv( colors, [1.0, 0.8, 0.3]);
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm2));
     giant();
@@ -233,6 +267,11 @@ function render(){
     gl.uniform3fv( colors, [1.0, 0.7, 0.5]);
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm3));
     stars2();
+
+    //draw points when canvas clicked
+    gl.uniform3fv( colors, [1.0, 0.0, 0.0]);
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm3));
+    drawPoints();
 
     requestAnimationFrame(render);
 }
@@ -279,4 +318,12 @@ function giant(){
     gl.drawArrays(gl.TRIANGLES,84,3);
     gl.drawArrays(gl.TRIANGLES,87,3);
     gl.drawArrays(gl.TRIANGLES,90,3);
+}
+
+function drawPoints(){
+    if(index != 0){
+        for(var i=0;i<index;i++){
+            gl.drawArrays( gl.TRIANGLE_FAN, 93+i*6, 6);
+        }
+    }
 }
